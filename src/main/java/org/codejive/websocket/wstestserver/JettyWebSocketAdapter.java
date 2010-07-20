731728507deb60aaae4d3b8f803aba952f14f636
Telefonica1 <Telefonica1@TLDAM3507>
2010-07-20 18:49:43 +0200

    Moved the message handling code from the JettyWebSocketAdapter to RwsSession making the adapter much simpler.

--- src/main/java/org/codejive/websocket/wstestserver/JettyWebSocketAdapter.java
+++ src/main/java/org/codejive/websocket/wstestserver/JettyWebSocketAdapter.java
@@ -1,19 +1,12 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package org.codejive.websocket.wstestserver;
 
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import org.codejive.rws.RwsContext;
 import org.codejive.rws.RwsObject;
-import org.codejive.rws.RwsRegistry;
 import org.codejive.rws.RwsSession;
 import org.codejive.rws.RwsWebSocketAdapter;
 import org.eclipse.jetty.websocket.WebSocket.Outbound;
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -39,6 +32,7 @@ public class JettyWebSocketAdapter implements RwsWebSocketAdapter {
     @Override
     public void onConnect() {
         session = context.addSession(this);
+        RwsSession.setInstance(session);
         RwsObject obj = context.getRegistry().getObject("Session");
         context.getRegistry().register(obj, session, "session", session);
     }
@@ -46,20 +40,11 @@ public class JettyWebSocketAdapter implements RwsWebSocketAdapter {
     @Override
     public void onMessage(String msg) {
         log.info(this + " onMessage: " + msg);
+        RwsSession.setInstance(session);
         try {
             JSONParser parser = new JSONParser();
             JSONObject info = (JSONObject) parser.parse(msg);
-            String to = (String) info.get("to");
-            if (to == null || "sys".equals(to)) {
-                // The message is for the server
-                doCall(info);
-            } else if ("all".equals(to)) {
-                // Send the message to all connected sockets
-                context.sendAll(session.getId(), info, false);
-            } else {
-                // Send the message to the indicated socket
-                context.sendTo(session.getId(), to, info);
-            }
+            session.handleMessage(info);
         } catch (ParseException ex) {
             log.error("Couldn't parse incoming message", ex);
         } catch (IOException ex) {
@@ -70,6 +55,7 @@ public class JettyWebSocketAdapter implements RwsWebSocketAdapter {
     @Override
     public void onDisconnect() {
         log.info(this + " onDisconnect");
+        RwsSession.setInstance(session);
         session.disconnect();
     }
 
@@ -87,53 +73,5 @@ public class JettyWebSocketAdapter implements RwsWebSocketAdapter {
     @Override
     public void sendMessage(String msg) throws IOException {
         outbound.sendMessage(msg);
-    }
-
-    private void doCall(JSONObject info) throws IOException {
-        String id = (String) info.get("id"); // If null the caller is not interested in the result!
-        String obj = (String) info.get("object");
-        String method = (String) info.get("method");
-        Object params = (Object) info.get("params");
-
-        Object[] args = null;
-        // Convert parameter map to array
-        if (params != null && params instanceof JSONArray) {
-            JSONArray p = (JSONArray) params;
-            args = new Object[p.size()];
-            for (int i = 0; i < p.size(); i++) {
-                args[i] = p.get(i);
-            }
-        }
-
-        try {
-            Object result = context.getRegistry().call(session, obj, method, args);
-            if (id != null) {
-                session.send("sys", newCallResult(id, result));
-            }
-        } catch (InvocationTargetException ex) {
-            log.error("Remote object returned an error", ex);
-            if (id != null) {
-                session.send("sys", newCallException(id, ex));
-            }
-        } catch (Throwable th) {
-            log.error("Rws Call failed", th);
-            if (id != null) {
-                session.send("sys", newCallException(id, th));
-            }
-        }
-    }
-
-    private JSONObject newCallResult(String id, Object data) {
-        JSONObject obj = new JSONObject();
-        obj.put("id", id);
-        obj.put("result", data);
-        return obj;
-    }
-
-    private JSONObject newCallException(String id, Throwable th) {
-        JSONObject obj = new JSONObject();
-        obj.put("id", id);
-        obj.put("exception", th.toString());
-        return obj;
     }
 }
